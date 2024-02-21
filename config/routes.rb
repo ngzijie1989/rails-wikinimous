2a0ea1a72942dda2943d42b0f71c65f98234875a
ng <ng_zi_jie@hotmail.com>
2024-02-21 22:36:26 +0800

    complete

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  get "articles", to: "articles#index"
+  get "articles/new", to: "articles#new", as: "new_article"
+  post "articles", to: "articles#create"
+  get "articles/:id", to: "articles#show", as: "article"
 end
